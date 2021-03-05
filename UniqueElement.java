a3c1e95307d290f3bd123d4260bda577c0761cd7
Sweta-sweta <60968698+Sweta-sweta@users.noreply.github.com>
2021-03-05 16:06:29 +0530

    Add files via upload

---
+++ UniqueElement.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class UniqueElement
+{
+  static int unique(int arr[],int size)
+  { 
+   int unq_ele=arr[0];
+   for(int i=1;i<size;i++)
+    unq_ele=unq_ele^arr[i];
+	return unq_ele;
+	}
+	public static void main(String arg[])
+	{
+	  int ar[]={1,2,3,1,2};
+	  int n=ar.length;
+	  System.out.println(unique(ar,n));
+	}
+}
+   
